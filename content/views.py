4ba595bb6aced3747f9a13e13a988361448a3c1e
aliasav <saurabhav24@gmail.com>
2020-07-04 17:20:15 +0530

    add home page

--- content/views.py
+++ content/views.py
@@ -11,7 +11,7 @@ from django.shortcuts import render
 def home_view(request):
     '''renders home page
     '''    
-    template = 'home/home.html'
+    template = 'home.html'
     context = {}
     return render(request, template, context)
 
